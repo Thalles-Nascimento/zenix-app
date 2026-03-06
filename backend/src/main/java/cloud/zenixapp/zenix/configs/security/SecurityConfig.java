fa5d06befb86d2ebe2b1609001c81ab36b23cb8d
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-03-06 16:57:07 -0300

    Autorizacoes

--- backend/src/main/java/cloud/zenixapp/zenix/configs/security/SecurityConfig.java
+++ backend/src/main/java/cloud/zenixapp/zenix/configs/security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:5173", "https://app.zenixapp.cloud"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://app.zenixapp.cloud"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
