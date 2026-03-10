0c8dad7651f8ab215499d9eaea3aae11df6b2613
Thalles_Nascimento <139908622+Thalles-Ribeiro-Nascimento@users.noreply.github.com>
2026-03-10 10:38:29 -0300

    Alterando o registro do usuario

--- backend/src/main/java/cloud/zenixapp/zenix/services/UsuarioService.java
+++ backend/src/main/java/cloud/zenixapp/zenix/services/UsuarioService.java
@@ -76,7 +76,7 @@ public class UsuarioService {
             return null;
         }
         String encryptPassword = new BCryptPasswordEncoder().encode(userRegister.senha());
-        if (userRegister.unidade() == null){
+        if (userRegister.unidade() == 0){
             UsuarioResponseDTO usuario = usuarioMapper.usuarioResponseDTO(
                     usuarioRepository
                             .save(new Usuarios(userRegister.nome(), userRegister.email(), encryptPassword, userRegister.cpf(), userRegister.grupo()))
