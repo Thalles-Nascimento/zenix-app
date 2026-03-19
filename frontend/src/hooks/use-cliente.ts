092fde6d182278b79108e611487dcbd8c107bc6b
Thalles Nascimento <thalles.nascimentorj@gmail.com>
2026-03-19 07:56:38 -0300

    refactor: Refatorando o Type ClienteDTO do use-cliente que e publico

--- frontend/src/hooks/use-cliente.ts
+++ frontend/src/hooks/use-cliente.ts
@@ -1,10 +1,10 @@
 import { useState } from "react"
 import { atualizarRetornoService, buscarClientesPorTelefoneService, criarClienteService } from "../services/cliente-service"
-import type { ClienteDTO } from "../services/cliente-service"
+import type { ClienteDTOGeral } from "@/types/cliente"
 
 export function useCliente() {
     const [buscando, setBuscando] = useState(false)
-    const [clientes, setClientes] = useState<ClienteDTO[]>([])
+    const [clientes, setClientes] = useState<ClienteDTOGeral[]>([])
 
     const buscarClientes = async (telefone: string) => {
         const numeroCliente = telefone.replace(/\D/g, "")
