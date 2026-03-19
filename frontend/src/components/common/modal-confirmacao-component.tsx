ec6c5ed23d074d1355795f05d7ebb77bdd0cd5af
Thalles Nascimento <thalles.nascimentorj@gmail.com>
2026-03-19 07:19:00 -0300

    feat: Novo modal para confirmar exclusao

---
+++ frontend/src/components/common/modal-confirmacao-component.tsx
@@ -0,0 +1,27 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog"
+import { Botao } from "./botao"
+
+interface Props {
+    open: boolean
+    titulo?: string
+    mensagem: string
+    onConfirmar: () => void
+    onCancelar: () => void
+}
+
+export function ModalConfirmacao({ open, titulo = "Confirmar ação", mensagem, onConfirmar, onCancelar }: Props) {
+    return (
+        <Dialog open={open} onOpenChange={onCancelar}>
+            <DialogContent className="bg-black border-gray-700 text-white w-[calc(100vw-2rem)] max-w-sm notranslate">
+                <DialogHeader>
+                    <DialogTitle className="text-white notranslate">{titulo}</DialogTitle>
+                </DialogHeader>
+                <p className="text-gray-300 text-sm mt-1">{mensagem}</p>
+                <div className="flex flex-col gap-2 mt-4">
+                    <Botao texto="Confirmar" color="delete" click={onConfirmar} />
+                    <Botao texto="Cancelar" color="cancel" click={onCancelar} />
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}
