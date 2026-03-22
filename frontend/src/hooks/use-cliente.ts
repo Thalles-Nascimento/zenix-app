24ad389bbcc9994406c3c43cf1f0c6cf29b433a8
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-03-22 00:07:37 -0300

    refactor: Padronizando a interface ClienteDTO

--- frontend/src/hooks/use-cliente.ts
+++ frontend/src/hooks/use-cliente.ts
@@ -1,10 +1,10 @@
 import { useState } from "react"
 import { atualizarRetornoService, buscarClientesPorTelefoneService, criarClienteService } from "../services/cliente-service"
-import type { ClienteDTOGeral } from "@/types/cliente"
+import type { ClienteDTO } from "@/types/cliente"
 
 export function useCliente() {
     const [buscando, setBuscando] = useState(false)
-    const [clientes, setClientes] = useState<ClienteDTOGeral[]>([])
+    const [clientes, setClientes] = useState<ClienteDTO[]>([])
 
     const buscarClientes = async (telefone: string) => {
         const numeroCliente = telefone.replace(/\D/g, "")
