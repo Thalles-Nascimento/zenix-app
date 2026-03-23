1d63627c4c658c0ff98448810c9d99ca5e7cfbdb
Thalles Nascimento <thalles.nascimentorj@gmail.com>
2026-03-23 17:33:50 -0300

    refactor: Estabelecendo o tamanho minimo de 6 digitos para Senha

--- backend/src/main/java/cloud/zenixapp/zenix/models/dtos/requests/UsuarioLoginDTO.java
+++ backend/src/main/java/cloud/zenixapp/zenix/models/dtos/requests/UsuarioLoginDTO.java
@@ -2,6 +2,7 @@ package cloud.zenixapp.zenix.models.dtos.requests;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UsuarioLoginDTO(
         @Email(message = "E-mail inválido")
@@ -9,6 +10,7 @@ public record UsuarioLoginDTO(
         String email,
 
         @NotNull(message = "Campo não pode ser nulo")
+        @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
         String senha
 ) {
 
