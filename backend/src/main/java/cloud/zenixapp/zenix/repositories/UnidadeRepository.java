9f3a19dc55651ddb8cf43406e3a97347796d97a6
Thalles Nascimento <thalles.nascimentorj@gmail.com>
2026-03-25 13:56:15 -0300

    refactor: Refatorando o UnidadeRepository para querys com filtro Tenant

--- backend/src/main/java/cloud/zenixapp/zenix/repositories/UnidadeRepository.java
+++ backend/src/main/java/cloud/zenixapp/zenix/repositories/UnidadeRepository.java
@@ -6,13 +6,19 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UnidadeRepository extends JpaRepository<Unidades, Long> {
 
     @Modifying
-    @Query(value = "UPDATE Unidades SET status = -1 WHERE id = :id")
-    void deleteLogico(@Param("id") Long id);
+    @Query(value = "UPDATE unidades SET unidade_status = -1 WHERE unidade_id = :id AND tenant_id = :tenantId", nativeQuery = true)
+    void deleteLogico(@Param("id") Long id, @Param("tenantId") String tenantId);
 
     @Modifying
-    @Query(value = "UPDATE Unidades SET status = 1 WHERE id = :id")
-    void ativarUnidade(@Param("id") Long id);
+    @Query(value = "UPDATE unidades SET unidade_status = 1 WHERE unidade_id = :id AND tenant_id = :tenantId", nativeQuery = true)
+    void ativarUnidade(@Param("id") Long id, @Param("tenantId")String tenantId);
+
+    @Query(value = "SELECT * FROM unidades WHERE unidade_nome = :nome AND tenant_id = :tenantId", nativeQuery = true)
+    Optional<Unidades> buscarUnidadesPorTenant(@Param("nome") String nome, @Param("tenantId") String tenantId);
 }
