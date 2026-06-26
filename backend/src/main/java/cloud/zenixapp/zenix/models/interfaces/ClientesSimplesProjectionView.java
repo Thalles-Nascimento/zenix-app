70756fc46d8fa9febf104fb6aac52f6505b8e26f
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-06-26 17:01:44 -0300

    feat: adicionando uma nova projecao para cliente

--- backend/src/main/java/cloud/zenixapp/zenix/models/interfaces/ClientesSimplesProjectionView.java
+++ backend/src/main/java/cloud/zenixapp/zenix/models/interfaces/ClientesSimplesProjectionView.java
@@ -1,16 +1,11 @@
 package cloud.zenixapp.zenix.models.interfaces;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public interface ClientesSimplesProjectionView {
     String getId();
     String getNome();
     String getTelefone();
-    String getPlanosId();
-    LocalDate getDataRenovacao();
     LocalDateTime getUpdatedAt();
-    int getAtendimentoMes();
-    int getRetorno();
     int getStatus();
 }
