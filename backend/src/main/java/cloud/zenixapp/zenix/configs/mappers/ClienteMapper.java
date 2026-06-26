2e606f8b8629886093038d71e8d44a08e0fa1748
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-06-26 17:01:22 -0300

    feat: adicionando um novo metodo na interface para uma projecao mais simples do cliente

--- backend/src/main/java/cloud/zenixapp/zenix/configs/mappers/ClienteMapper.java
+++ backend/src/main/java/cloud/zenixapp/zenix/configs/mappers/ClienteMapper.java
@@ -4,6 +4,7 @@ import cloud.zenixapp.zenix.models.dtos.requests.ClienteUpdateRequestDTO;
 import cloud.zenixapp.zenix.models.dtos.responses.ClienteResponseDTO;
 import cloud.zenixapp.zenix.models.entities.Clientes;
 import cloud.zenixapp.zenix.models.interfaces.ClientesProjectionView;
+import cloud.zenixapp.zenix.models.interfaces.ClientesSimplesProjectionView;
 import org.mapstruct.*;
 
 import java.util.List;
@@ -27,9 +28,9 @@ public interface ClienteMapper {
     @Mapping(target = "createdAt", ignore = true)
     void atualizarCliente(@MappingTarget Clientes clientes, ClienteUpdateRequestDTO clienteUpdateRequestDTO);
 
-    @Mapping(target = "telefoneCliente.telefoneCliente", source = "telefone")
+    @Mapping(target = "telefoneCliente", ignore = true)
     @Mapping(target = "totalRetornos", source = "retorno")
-    @Mapping(target = "planos.id", source = "planosId")
+    @Mapping(target = "planos", ignore = true)
     @Mapping(target = "nomeCliente", source = "nome")
     @Mapping(target = "atendimentosMes", source = "atendimentoMes")
     @Mapping(target = "updatedAt", source = "updatedAt")
@@ -37,4 +38,16 @@ public interface ClienteMapper {
     @Mapping(target = "deletedAt", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     Clientes toClientes(ClientesProjectionView clientesView);
+
+    @Mapping(target = "totalRetornos", ignore = true)
+    @Mapping(target = "planos", ignore = true)
+    @Mapping(target = "dataRenovacao", ignore = true)
+    @Mapping(target = "atendimentosMes", ignore = true)
+    @Mapping(target = "tenant", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "telefoneCliente", ignore = true)
+    @Mapping(target = "nomeCliente", source = "nome")
+    @Mapping(target = "updatedAt", source = "updatedAt")
+    Clientes toClienteSimples(ClientesSimplesProjectionView clientesSimplesProjectionView);
 }
---
+++ backend/src/main/java/cloud/zenixapp/zenix/models/interfaces/ClientesSimplesProjectionView.java
@@ -0,0 +1,16 @@
+package cloud.zenixapp.zenix.models.interfaces;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public interface ClientesSimplesProjectionView {
+    String getId();
+    String getNome();
+    String getTelefone();
+    String getPlanosId();
+    LocalDate getDataRenovacao();
+    LocalDateTime getUpdatedAt();
+    int getAtendimentoMes();
+    int getRetorno();
+    int getStatus();
+}
