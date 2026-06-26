25747aba10b9613a4cad68177a6584bf4f34f3df
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-06-26 17:02:41 -0300

    feat: adicionando novos metodos para buscar clientes

--- backend/src/main/java/cloud/zenixapp/zenix/repositories/ClienteRepository.java
+++ backend/src/main/java/cloud/zenixapp/zenix/repositories/ClienteRepository.java
@@ -3,6 +3,7 @@ package cloud.zenixapp.zenix.repositories;
 import cloud.zenixapp.zenix.models.entities.Clientes;
 import cloud.zenixapp.zenix.models.entities.TelefoneCliente;
 import cloud.zenixapp.zenix.models.interfaces.ClientesProjectionView;
+import cloud.zenixapp.zenix.models.interfaces.ClientesSimplesProjectionView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.NativeQuery;
@@ -29,11 +30,24 @@ public interface ClienteRepository extends JpaRepository<Clientes, String> {
                     "FROM clientes c " +
                     "LEFT JOIN telefones_clientes tc ON c.telefone_id = tc.id " +
                     "WHERE c.cliente_nome = :nome AND c.tenant_id = :tenantId AND c.cliente_status = 1")
-    ClientesProjectionView findByName(@Param("nome") String nome, @Param("tenantId") String tenantId);
+    Optional<ClientesProjectionView> findByName(@Param("nome") String nome, @Param("tenantId") String tenantId);
 
-    @Query(value = "SELECT * FROM clientes WHERE cliente_nome LIKE %:nome% AND cliente_status = 1", nativeQuery = true)
-    List<Clientes> findByNameContaining(@Param("nome") String nome);
-
+    @NativeQuery(
+            value = "SELECT " +
+                    "c.id AS id," +
+                    "c.cliente_nome AS nome," +
+                    "tc.telefone_cliente AS telefone," +
+                    "c.cliente_data_renovacao AS dataRenovacao," +
+                    "c.cliente_atendimentos_mes AS atendimentoMes," +
+                    "c.cliente_retorno AS retorno," +
+                    "c.updated_at AS updatedAt," +
+                    "c.planos_id AS planosId," +
+                    "c.cliente_status AS status " +
+                    "FROM clientes c " +
+                    "LEFT JOIN telefones_clientes tc ON c.telefone_id = tc.id " +
+                    "WHERE c.cliente_nome LIKE %:nome% AND c.tenant_id = :tenantId AND c.cliente_status = 1")
+    List<ClientesProjectionView> findByNameContaining(@Param("nome") String nome, @Param("tenantId") String tenantId);
+//WHERE cliente_nome LIKE %:nome% AND cliente_status = 1
     @Modifying
     @NativeQuery("UPDATE clientes SET atendimentosMes = 0 WHERE tenant_id = :tenantId planos_id IS NOT NULL AND DAY(cliente_data_renovacao) = :dia")
     void resetarAtendimentosMes(@Param("dia") int dia, @Param("tenantId") String tenantId);
@@ -104,5 +118,20 @@ public interface ClienteRepository extends JpaRepository<Clientes, String> {
                     "LEFT JOIN telefones_clientes tc ON c.telefone_id = tc.id " +
                     "WHERE c.id = :id AND c.tenant_id = :tenantId AND c.cliente_status = 1")
     Optional<ClientesProjectionView> findById(@Param("id") String id, @Param("tenantId") String tenantId);
+
+    @NativeQuery(
+            value = "SELECT " +
+                    "c.id AS id," +
+                    "c.cliente_nome AS nome," +
+                    "tc.telefone_cliente AS telefone," +
+                    "c.cliente_data_renovacao AS dataRenovacao," +
+                    "c.cliente_atendimentos_mes AS atendimentoMes," +
+                    "c.cliente_retorno AS retorno," +
+                    "c.updated_at AS updatedAt," +
+                    "c.cliente_status AS status " +
+                    "FROM clientes c " +
+                    "LEFT JOIN telefones_clientes tc ON c.telefone_id = tc.id " +
+                    "WHERE c.id = :id AND c.tenant_id = :tenantId")
+    Optional<ClientesSimplesProjectionView> findByIdSimples(@Param("id") String id, @Param("tenantId") String tenantId);
 
 }
