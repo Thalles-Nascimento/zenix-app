1434b6c4eac0b74a54c0ff8bdd0c3ddab4448106
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-07-16 16:47:47 -0300

    refactor: adicionando o metodo getUpdatedAt

--- backend/src/main/java/cloud/zenixapp/zenix/models/interfaces/FormaPagamentoView.java
+++ backend/src/main/java/cloud/zenixapp/zenix/models/interfaces/FormaPagamentoView.java
@@ -1,8 +1,11 @@
 package cloud.zenixapp.zenix.models.interfaces;
 
 
+import java.time.LocalDateTime;
+
 public interface FormaPagamentoView {
     String getId();
     String getPagamento();
+    LocalDateTime getUpdatedAt();
     int getStatus();
 }
