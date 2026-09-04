ba9807c94330c66036ff0c14652f2feb0eb67519
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-09-04 12:52:37 -0300

    feat: criação do Dockerfile e Docker Compose para executar a aplicação de maneira padrão em qualquer máquina via container

--- backend/build.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Limpando e construindo o projeto..."
-mvn clean package -DskipTests
-Write-Host "Build completo!"
-
-Write-Host "Iniciando containirização"
-docker compose up --build -d
-
-Write-Host "Container criado! Aplicação rodando: http://localhost:8080/swagger-ui/index.html "
