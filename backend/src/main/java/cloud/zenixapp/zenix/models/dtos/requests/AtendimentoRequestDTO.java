602d82ff3bb295812a287dea154c609499b9c08b
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-09-16 12:53:45 -0300

    docs: Documentando os DTOs de Atendimento

--- backend/src/main/java/cloud/zenixapp/zenix/models/dtos/requests/AtendimentoRequestDTO.java
+++ backend/src/main/java/cloud/zenixapp/zenix/models/dtos/requests/AtendimentoRequestDTO.java
@@ -6,7 +6,19 @@ import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
-
+/**
+ * <h2>
+ *     Record AtendimentoRequestDTO
+ * </h2>
+ * <p>
+ *     DTO para requisições do Domínio Atendimento.
+ * </p>
+ * @param descricao Nome do cliente do atendimento {@link NotNull} {@link Pattern}
+ * @param servico Lista de serviços do atendimento {@link NotNull}
+ * @param formaPagamento Forma de pagamento do atendimento {@link NotNull} {@link Pattern}
+ * @param observacao Observação do atendimento {@code Opcional}
+ * @param valor Valor total do atendimento {@link NotNull}
+ */
 public record AtendimentoRequestDTO (
         @NotNull(message = "Campo não pode ser nulo!")
         @Pattern(
--- backend/src/main/java/cloud/zenixapp/zenix/models/dtos/responses/atendimentos/AtendimentoResponseWriteDTO.java
+++ backend/src/main/java/cloud/zenixapp/zenix/models/dtos/responses/atendimentos/AtendimentoResponseWriteDTO.java
@@ -3,6 +3,24 @@ package cloud.zenixapp.zenix.models.dtos.responses.atendimentos;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * <h2>
+ *      Record AtendimentoResponseWriteDTO
+ * </h2>
+ * <p>
+ *     DTO de resposta do Domínio Atendimento usado para atualizar um atendimento.
+ * </p>
+ * @param id ID do atendimento
+ * @param descricao Nome do cliente do atendimento
+ * @param servico Lista de serviços do atendimento
+ * @param valor Valor total do atendimento
+ * @param formaPagamento Forma de pagamento do atendimento
+ * @param date Data do atendimento
+ * @param updatedAt Data e hora da última atualização do atendimento
+ * @param deletedAt Data e hora que o atendimento foi deletado
+ * @param observacao Observação do atendimento
+ * @param status
+ */
 public record AtendimentoResponseWriteDTO(
         String id,
 
