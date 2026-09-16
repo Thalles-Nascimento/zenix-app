a165917212a3acc634032bc74016ce4b80e6bdc4
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-09-16 12:18:05 -0300

    docs: Documentando o AtendimentoMapper e AtendimentoRepository

--- backend/src/main/java/cloud/zenixapp/zenix/configs/mappers/AtendimentoMapper.java
+++ backend/src/main/java/cloud/zenixapp/zenix/configs/mappers/AtendimentoMapper.java
@@ -7,9 +7,34 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
+/**
+ * <h2>
+ *     Interface para mapear um DTO para Entidade Atendimento.
+ * </h2>
+ * <p>
+ *     A interface é utilizada para realizar o mapeamento entre AtendimentoRequestDTO e Atendimento.
+ * </p>
+ *
+ * @see Mapper
+ * @version 1.0
+ * @author Thalles Nascimento
+ */
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface AtendimentoMapper {
 
+    /**
+     * <h2>
+     *     Método para atualizar um atendimento.
+     * </h2>
+     * <p>
+     *     Este método é utilizado para realizar o mapeamento do DTO enviado na requisição para a entidade Atendimento
+     *     ignorando campos que não podem ser atualizados.
+     * </p>
+     * @param atendimento Entidade que receberá os dados de AtendimentoRequestDTO.
+     * @param atendimentoRequestDTO DTO contendo os dados que serão mapeados para entidade Atendimento.
+     * @see Atendimento
+     * @see AtendimentoRequestDTO
+     */
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "tenant", ignore = true)
     @Mapping(target = "deletedAt", ignore = true)
@@ -18,9 +43,20 @@ public interface AtendimentoMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "status", ignore = true)
     @Mapping(target = "usuarios", ignore = true)
-    @Mapping(target = "observacao", source = "observacao")
     void atualizarAtendimento(@MappingTarget Atendimento atendimento, AtendimentoRequestDTO atendimentoRequestDTO);
 
+    /**
+     * <h2>
+     *     Método para mapear um Atendimento dado um DTO.
+     * </h2>
+     * <p>
+     *     Este método transforma um AtendimentoRequestDTO numa entidade Atendimento. É utilizado para inserir um atendimento no sistema.
+     * </p>
+     * @param atendimentoRequestDTO DTO contendo os dados que serão mapeados.
+     * @return {@link Atendimento}
+     * @see AtendimentoRequestDTO
+     * @see Atendimento
+     */
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "tenant", ignore = true)
     @Mapping(target = "deletedAt", ignore = true)
--- backend/src/main/java/cloud/zenixapp/zenix/repositories/AtendimentoRepository.java
+++ backend/src/main/java/cloud/zenixapp/zenix/repositories/AtendimentoRepository.java
@@ -3,7 +3,9 @@ package cloud.zenixapp.zenix.repositories;
 import cloud.zenixapp.zenix.models.dtos.responses.atendimentos.AtendimentoResponseDTO;
 import cloud.zenixapp.zenix.models.dtos.responses.atendimentos.AtendimentoResponseWriteDTO;
 import cloud.zenixapp.zenix.models.entities.Atendimento;
+import cloud.zenixapp.zenix.models.entities.Tenants;
 import cloud.zenixapp.zenix.models.entities.Usuarios;
+import cloud.zenixapp.zenix.services.AtendimentoService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.NativeQuery;
@@ -14,15 +16,66 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * <h2>
+ *      Repositório da Entidade Atendimento.
+ * </h2>
+ * <p>
+ *     O repositório é responsável por realizar as consultas no banco de dados do sistema.
+ * </p>
+ * @see JpaRepository
+ * @see Tenants
+ *
+ * @version 1.0
+ * @author Thalles Nascimento
+ */
 public interface AtendimentoRepository extends JpaRepository<Atendimento, String> {
 
-//    Lista o histórico por ID do barbeiro > atendimentoService.listarHistorico()
+    /**
+     * <h2>
+     *     Método para consultar o histórico de atendimentos de um usuário
+     * </h2>
+     * <p>
+     *     Este método busca no banco de dados os atendimentos de um usuário.
+     * </p>
+     * @param usuarios Entidade {@link Usuarios} usada para claúsula Where da consulta.
+     * @param tenant TenantID do inquilino que realizou a requisição - Type: {@code String}.
+     * @return List<{@link AtendimentoResponseDTO}> - Pode retornar uma lista vazia.
+     * @see Usuarios
+     * @see AtendimentoResponseDTO
+     */
     List<AtendimentoResponseDTO> findByUsuariosAndTenant(Usuarios usuarios, String tenant);
 
-//    Lista os atendimentos do dia - hoje ≥ atendimentoService.listarAtendimentosHoje()
+    /**
+     * <h2>
+     *     Método para consultar os atendimentos do dia de um usuário.
+     * </h2>
+     * <p>
+     *     Este método busca no banco de dados os atendimentos do dia de um usuário.
+     * </p>
+     * @param usuarios Entidade {@link Usuarios} usada para claúsula Where da consulta.
+     * @param date {@code String} Data do dia.
+     * @param tenant TenantID do inquilino que realizou a requisição - Type: {@code String}.
+     * @return List<{@link AtendimentoResponseDTO}> - Pode retornar uma lista vazia.
+     * @see Usuarios
+     * @see AtendimentoResponseDTO
+     */
     List<AtendimentoResponseDTO> findByUsuariosAndDateAndTenant(Usuarios usuarios, String date, String tenant);
 
-//  Ativa um atendimento
+
+    /**
+     * <h2>
+     *     Método para ativar um atendimento.
+     * </h2>
+     * <p>
+     *     Este método ativa um atendimento no banco de dados, realizando um Update na coluna Status e DeletedAt.
+     * </p>
+     * @param id 'ID' do atendimento que será ativado
+     * @param tenantId TenantID do inquilino que realizou a requisição - Type: {@code String}.
+     *
+     * @see Modifying
+     * @see NativeQuery
+     */
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @NativeQuery(
             value = "UPDATE atendimentos a " +
@@ -30,10 +83,33 @@ public interface AtendimentoRepository extends JpaRepository<Atendimento, String
                     "WHERE a.id = :id AND a.tenant_id = :tenantId")
     void ativarAtendimento(@Param("id") String id, @Param("tenantId") String tenantId);
 
-//    Lista todos os atendimentos por Tenants ≥ atendimentoService.listarTodosAtendimentos()
+    /**
+     * <h2>
+     *     Método para listar todos os atendimentos.
+     * </h2>
+     * <p>
+     *     Este método lista todos os atendimentos da barbearia.
+     * </p>
+     * @param tenant TenantID do inquilino que realizou a requisição - Type: {@code String}.
+     * @return List<{@link AtendimentoResponseDTO}> - Pode retornar uma lista vazia.
+     * @see AtendimentoResponseDTO
+     */
     List<AtendimentoResponseDTO> findAllByTenant(String tenant);
 
-//  Deleta/desativa um atendimento
+    /**
+     * <h2>
+     *     Método para deletar um atendimento.
+     * </h2>
+     * <p>
+     *     Este método é utilizado para realizar um delete lógico do atendimento.
+     * </p>
+     * @param id ID do atendimento que será deletado.
+     * @param deleteTime Data e hora que o atendimento foi deletado.
+     * @param tenantId TenantID do inquilino que realizou a requisição - Type: {@code String}.
+     * @see Modifying
+     * @see NativeQuery
+     * @see LocalDateTime
+     */
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @NativeQuery(
             value = "UPDATE atendimentos a " +
@@ -41,9 +117,38 @@ public interface AtendimentoRepository extends JpaRepository<Atendimento, String
                     "WHERE a.id = :id AND a.tenant_id = :tenantId")
     void deleteLogico(@Param("id") String id, @Param("deleteTime") LocalDateTime deleteTime, @Param("tenantId") String tenantId);
 
-//  Lista um atendimento por ID e Tenant
+    /**
+     * <h2>
+     *     Método para listar um atendimento por ID.
+     * </h2>
+     * <p>
+     *     Este método lista um atendimento pelo ID.
+     * </p>
+     * @param id ID do atendimento que será retornado
+     * @param tenant TenantID do inquilino que realizou a requisição - Type: {@code String}.
+     * @return Optional<{@link AtendimentoResponseWriteDTO}>.
+     * @see Optional
+     * @see AtendimentoResponseWriteDTO
+     */
     Optional<AtendimentoResponseWriteDTO> findByIdAndTenant(String id, String tenant);
 
+    /**
+     * <h2>
+     *     Método para listar um atendimento por ID.
+     * </h2>
+     * <h6>Obs: É utilizado para atualizar um atendimento.</h6>
+     * <p>
+     *     Este método lista um atendimento por ID.
+     * </p>
+     * @param id ID do atendimento que será retornado.
+     * @param tenant TenantID do inquilino que realizou a requisição - Type: {@code String}.
+     * @return Optional<{@link Atendimento}>.
+     *
+     * @see Atendimento
+     * @see AtendimentoService
+     * @see Optional
+     * @see Query
+     */
     @Query(value = "SELECT a FROM Atendimento a WHERE a.id = :id AND a.tenant = :tenant")
     Optional<Atendimento> findById(@Param("id") String id, @Param("tenant") String tenant);
 
